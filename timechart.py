0225e03e6e9974844fb709a033c0a48764a9be6a
BabyWrassler <junk@jeffhighsmith.com>
2020-09-13 21:48:37 -0400

    printing unusable cart lists

--- timechart.py
+++ timechart.py
@@ -37,6 +37,8 @@ tasks = {
 }
 
 dates_for_days = {
+    "mon": "2020-09-08",
+    "tue": "2020-09-09",
     "wed": "2020-09-10",
     "thu": "2020-09-11",
     "fri": "2020-09-12",
@@ -51,8 +53,15 @@ def formatDate(weekday, time_of_day):
     else:
         time = f"{time_of_day[0]}:{time_of_day[1:2]}:00"
     date = f"{dates_for_days.get(str(weekday))} {time}"
-    print(date)
+    # print(date)
     return date
+
+
+def formatTime(time):
+    if int(time) < 1300:
+        return f"{time[0:2]}:{time[2:]}"
+    else:
+        return f"{int(time[0:2])-12}:{time[2:]}"
 
 
 with open("input.csv", "r") as input_file:
@@ -60,9 +69,11 @@ with open("input.csv", "r") as input_file:
     events = list(reader)
     for row in events:
         row["Task"] = tasks[row["Task"]]
-        row["Time"] = f"{row['Task']}<br>{row['Start']} - {row['End']}"
-        row["Start"] = formatDate(row["Day"], row["Start"])
-        row["End"] = formatDate(row["Day"], row["End"])
+        row[
+            "Time"
+        ] = f"{row['Task']}<br>{formatTime(row['Start'])} - {formatTime(row['End'])}"
+        row["StartDate"] = formatDate(row["Day"], row["Start"])
+        row["EndDate"] = formatDate(row["Day"], row["End"])
         row["exhibit1"] = exhibits.get(row["exhibit1"])
         row["exhibit2"] = exhibits.get(row["exhibit2"])
         if row["exhibit2"]:
@@ -70,20 +81,18 @@ with open("input.csv", "r") as input_file:
         else:
             row["Exhibits"] = row["exhibit1"]
 
-print(events)
-
-edf = pd.DataFrame(events)
+all_events_df = pd.DataFrame(events)
 
 
 def plotSingleDay(ddf, day):
     print(day)
-    df = ddf.loc[edf["Day"] == day]
+    df = ddf.loc[all_events_df["Day"] == day]
     if len(df):
         print(df)
         fig = px.timeline(
             df,
-            x_start="Start",
-            x_end="End",
+            x_start="StartDate",
+            x_end="EndDate",
             y="Exhibits",
             color="Task",
             text="Time",
@@ -92,7 +101,7 @@ def plotSingleDay(ddf, day):
 
         fig.update_layout(
             title=f"{days[day]} Exhibit Cleaning",
-            xaxis_tickformat="%H:%M",
+            xaxis_tickformat="%-I:%M",
             font_color="blue",
             font_size=10,
             title_font_family="Times New Roman",
@@ -105,4 +114,21 @@ def plotSingleDay(ddf, day):
 
 
 for day in days.keys():
-    plotSingleDay(edf, day)
+    plotSingleDay(all_events_df, day)
+
+
+def printExhibitTimes(df):
+
+    for ex in exhibits.values():
+        # ex_df = edf.loc[edf["exhibit2"] == ex]
+        print(ex)
+        single_exhibit_df = df[df.isin([ex]).any(axis=1)]
+
+        if len(single_exhibit_df) > 0:
+            print(single_exhibit_df[["Day", "Exhibits", "Start", "End", "Task"]])
+
+
+carts_df = all_events_df.loc[all_events_df["Task"] != tasks["wd"]]
+print(carts_df)
+
+printExhibitTimes(carts_df)
